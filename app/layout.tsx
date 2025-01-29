ad32de3c89bbc2e785b979c12583b67896b30537
Wicho <54341712+Wichosu@users.noreply.github.com>
2025-01-29 07:58:10 -0600

    Atemtting with layour.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
-import "./global.css";
+import "./layout.css";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
