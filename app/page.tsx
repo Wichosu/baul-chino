ac4ef267f81d2a6a54238e5c66d308a60507342e
Wicho <54341712+Wichosu@users.noreply.github.com>
2025-01-28 21:00:00 -0600

    Navbar addded and working on mobile and desktop, details can be fixed in the future

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import CardsContainer from "./components/CardsContainer";
 import Footer from "./components/Footer";
@@ -5,8 +6,9 @@ import Footer from "./components/Footer";
 export default function Home() {
   return (
     <>
-      <Hero>
-        Baúl Chino tiene como propósito ser una página con material didáctico para apoyar el aprendizaje del idioma Chino Mandarín. 好好学习, 天天向上.
+      <Navbar />
+      <Hero title="Inicio">
+        Baúl Chino tiene como propósito ser una página con material didáctico para apoyar el aprendizaje del idioma Chino Mandarín para los hispanohablantes. 好好学习, 天天向上.
       </Hero>
       <CardsContainer />
       <Footer />
