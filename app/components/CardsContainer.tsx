53e8e4a90a0aad3d6200675702bb94a476ef01b9
Wicho <54341712+Wichosu@users.noreply.github.com>
2025-02-06 18:28:23 -0600

    Remove HSK books from git, and added all progress done for libros-hsk in this commit

--- app/components/CardsContainer.tsx
+++ app/components/CardsContainer.tsx
@@ -2,16 +2,35 @@
 import styled from "styled-components"
 import Card from "./Card"
 import ankiHsk from "../images/anki-menu.png"
+import hskBooks from "../images/hsk-books.png"
+import { StaticImageData } from "next/image"
 
-const cardsList = [
+interface Card {
+  title: string,
+  imgAlt: string,
+  img: StaticImageData,
+  description: string,
+  linkHref: string,
+  linkName: string
+}
+
+const cardsList: Card[] = [
   {
     title: 'Mazos Anki HSK 1-4',
     imgAlt: 'Menú de Anki mostrando una lista de mazos',
     img: ankiHsk,
-    description: 'Colección de mazos Anki con vocabulario del HSK 1 al HSK 3',
+    description: 'Colección de mazos Anki con vocabulario del HSK 1 al HSK 4',
     linkHref: 'mazos-anki',
     linkName: 'Mazos Anki'
   },
+  {
+    title: 'Libros HSK',
+    imgAlt: 'Libros HSK del HSK 1 al HSK 6, los libros se ven ordenados y apilados',
+    img: hskBooks,
+    description: 'Descarga los libros HSK del 1 al 6 de manera gratuita',
+    linkHref: '/libros-hsk',
+    linkName: 'Libros HSK'
+  }
 ]
 
 export default function CardsContainer() {
@@ -37,4 +56,9 @@ export default function CardsContainer() {
 const Container = styled.div`
   width: 85%;
   margin: 0 auto;
+
+  @media (min-width: 768px) {
+    display: flex;
+    gap: 40px;
+  }
 `;
---
+++ app/libros-hsk/page.tsx
@@ -0,0 +1,33 @@
+import Navbar from "../components/Navbar"
+import Hero from "../components/Hero"
+import ListContainer from "./components/ListContainer"
+import Footer from "../components/Footer"
+import Link from "next/link"
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero title="Libros HSK">
+        Descarga todos los libros HSK, desde HSK 1 hasta HSK 6, en formato PDF. La descarga es totalmente
+        gratuita y sin anuncios o links molestos. 
+        <br />
+        <br />
+        ADVERTENCIA: La descarga del libro puede tomar unos
+        minutos dependiendo de la calidad de tu Internet. 
+        <br />
+        <br />
+        CARPETA DE DRIVE: 
+        <Link
+          style={{marginLeft: "5px", color: "blue"}}
+          href={'https://drive.google.com/drive/folders/1sokLkXNydcG5jzs-i639_38LU3bzga1f?usp=sharing'}
+          target="_blank"
+        >
+          presiona para visitar la carpeta de Google Drive
+        </Link>
+      </Hero>
+      <ListContainer />
+      <Footer />
+    </>
+  )
+}
