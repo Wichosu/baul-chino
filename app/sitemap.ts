e16e753dbd89645b8bc4c23b5d65de845d025cb2
Wicho <54341712+Wichosu@users.noreply.github.com>
2025-02-07 11:17:26 -0600

    added /libros-hsk to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -7,6 +7,9 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     },
     {
       url: `${process.env.NEXT_PUBLIC_BASE_URL}/mazos-anki`
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/libros-hsk`
     }
   ]
 }
