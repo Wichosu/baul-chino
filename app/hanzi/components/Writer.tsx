f71be3135044373f68587b11deeebb9dfa9a1786
Wicho <54341712+Wichosu@users.noreply.github.com>
2025-03-22 13:39:21 -0600

    Added ime and filter of roman and puntuation chars

--- app/hanzi/components/Writer.tsx
+++ app/hanzi/components/Writer.tsx
@@ -1,18 +1,20 @@
 "use client"
 import HanziWriter from "hanzi-writer"
-import { useCallback, useEffect, useRef, useState } from "react"
+import { useCallback, useEffect, useRef } from "react"
 import styled from "styled-components"
 import { cleanUpCanvas, createHanziBackground } from "../utils/renderHanziUtils"
+import { createSimpleIme } from "simple-ime"
+import InputWriter from "./InputWriter"
+import { SimpleImeInstance } from "simple-ime/dist/src/types"
+
+interface ISimpleImeInstanceExtended extends SimpleImeInstance {
+  chiMode?: boolean
+}
 
 export default function Writer() {
   const canvasRef = useRef<HTMLDivElement>(null!)
-  const [hanzi, setHanzi] = useState<string>('汉字')
 
-  const renderHanzi = useCallback(async () => {
-    const romanRegex = '[A-Z]|[a-z]'
-
-    if (hanzi.match(romanRegex)) return
-
+  const renderHanzi = useCallback(async (hanzi: string) => {
     //Cleanup previous hanzi
     cleanUpCanvas(canvasRef.current.childNodes)
 
@@ -20,6 +22,13 @@ export default function Writer() {
     const hanziArray = hanzi.split('')
 
     hanziArray.map((hanzi) => {
+      //Filter Roman letters and Punctuation simbols
+      const romanRegex = /[A-Z]|[a-z]/
+      const punctRegex = /[^\p{Script=Han}\p{L}\p{N}]/u
+
+      if (hanzi.match(romanRegex)) return
+      if (hanzi.match(punctRegex)) return
+
       //Create Hanzi background
       const background = createHanziBackground(canvasRef.current)
 
@@ -30,21 +39,25 @@ export default function Writer() {
       })
         .loopCharacterAnimation()
     })
-  }, [hanzi])
+  }, [])
 
   useEffect(() => {
-    renderHanzi()
-  }, [renderHanzi])
+    const ime: ISimpleImeInstanceExtended = createSimpleIme()
+
+    ime.turnOn()
+
+    ime.chiMode = true
+
+    return () => {
+      ime.dispose()
+    }
+  }, [])
 
   return (
     <>
       <Container>
         <CanvasDiv ref={canvasRef} />
-        <Input 
-          type='text' 
-          onChange={(e) => setHanzi(e.target.value)} 
-          placeholder="Escribe aquí tus hanzi 汉字" 
-        />
+        <InputWriter renderHanzi={renderHanzi} />
       </Container>
     </>
   )
@@ -66,17 +79,3 @@ const CanvasDiv = styled.div`
   gap: 20px;
 `
 
-const Input = styled.input`
-  width: 100%;
-  margin-bottom: 20px;
-  padding-top: 5px;
-  padding-bottom: 5px;
-  padding-left: 10px;
-  padding-right: 10px;
-  border-radius: 4px;
-  border: solid 1px #333333;
-
-  &:focus {
-    border: none;
-  }
-`
