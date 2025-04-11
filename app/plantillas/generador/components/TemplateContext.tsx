7b0ee89e2a42c0963bc251aa6fb371ca906d4a94
Wichosu <luiscalderonmiranda@gmail.com>
2025-04-10 18:53:32 -0600

    Working on Header interactive styles

--- app/plantillas/generador/components/TemplateContext.tsx
+++ app/plantillas/generador/components/TemplateContext.tsx
@@ -5,23 +5,36 @@ type Props = {
   children: ReactNode
 }
 
+export type Title = {
+  name: string
+  marginRight: number
+}
+
 type TemplateContext = {
   filename: RefObject<string>
   gridGap: number
   squareCount: number
   squareSize: number
+  titles: Array<Title>
   setGridGap: Dispatch<SetStateAction<number>>
   setSquareCount: Dispatch<SetStateAction<number>>
   setSquareSize: Dispatch<SetStateAction<number>>
+  setTitles: Dispatch<SetStateAction<Array<Title>>>
 }
 
 const TemplateContext = createContext<TemplateContext | null>(null)
 
 export default function TemplateContextProvider({ children }: Props) {
   const [gridGap, setGridGap] = useState(0)
+
   const [squareCount, setSquareCount] = useState(330)
   const [squareSize, setSquareSize] = useState(12)
+
   const filename = useRef('')
+
+  const [titles, setTitles] = useState<Title[]>([])
+
+  console.log(titles)
 
   return (
     <TemplateContext.Provider 
@@ -30,9 +43,11 @@ export default function TemplateContextProvider({ children }: Props) {
         gridGap,
         squareCount,
         squareSize,
+        titles,
         setGridGap,
         setSquareCount,
-        setSquareSize
+        setSquareSize,
+        setTitles
       }}
     >
       {children}
--- app/plantillas/generador/components/TemplateGenerator.tsx
+++ app/plantillas/generador/components/TemplateGenerator.tsx
@@ -7,6 +7,7 @@ import TemplateDownload from "./TemplateDownload";
 import GridForm from "./forms/GridForm";
 import SquareForm from "./forms/SquareForm";
 import TemplatePreview from "./TemplatePreview";
+import TitleForm from "./forms/TitleForm";
 
 export default function TemplateGenerator() {
   const targetRef = useRef<HTMLDivElement>(null!)
@@ -21,6 +22,7 @@ export default function TemplateGenerator() {
         <TemplateDownload toPDF={toPDF} />
         <GridForm />
         <SquareForm />
+        <TitleForm />
         <TemplatePreview targetRef={targetRef} />
       </TemplateContextProvider>
     </Container>
