90e7a1e50af395972a79cd3c02a6e8f639f29029
desarrollocb <desarrollo01@cbinformatica.net>
2025-05-23 18:38:07 -0600

    Plantillas tailwind

--- src/app/[locale]/plantillas/components/Templates/TemplateCard.tsx
+++ src/app/[locale]/plantillas/components/Templates/TemplateCard.tsx
@@ -1,5 +1,3 @@
-"use client"
-import styled from "styled-components"
 import Link from "next/link"
 import Image from "next/image"
 import downloadIcon from "../../assets/download.svg";
@@ -13,74 +11,21 @@ type Props = {
 
 export default function TemplateCard({ textAlt, img, pdfUrl }: Props) {
   return (
-    <LinkWrapper href={pdfUrl} download target="_blank">
-      <Figure>
-        <StyledImage alt={textAlt} src={img} />
-      </Figure>
-      <TextWrapper>
-        <Span>{textAlt}</Span>
-        <IconFigure>
-          <StyledImage alt="Download icon" src={downloadIcon} />
-        </IconFigure>
-      </TextWrapper>
-    </LinkWrapper>
+    <article className="inline-block w-fit no-underline my-2.5 snap-center lg:mx-auto">
+      <figure className="mx-auto">
+        <Image alt={textAlt} src={img} width={500} height={500} className="aspect-square object-contain" />
+      </figure>
+      <Link
+        href={pdfUrl}
+        download
+        target="_blank"
+        className="flex items-center w-fit bg-blue-600 mt-1 mx-auto py-2.5 px-5 border-none rounded-lg transition hover:bg-blue-700"
+      >
+        <span className="text-xl text-white font-medium">{textAlt}</span>
+        <figure>
+          <Image alt="Download icon" src={downloadIcon} width={20} height={20} className="aspect-square object-contain" />
+        </figure>
+      </Link>
+    </article>
   )
 }
-
-const LinkWrapper = styled(Link)`
-  display: inline-block;
-  width: fit-content;
-  text-decoration: none;
-  margin-top: 10px;
-  margin-bottom: 10px;
-  scroll-snap-align: start;
-
-  @media (min-width: 768px) {
-    margin-left: auto;
-    margin-right: auto;
-  }
-`
-
-const Figure = styled.figure`
-  width: 300px;
-  height: 400px;
-  margin: 0 auto;
-`
-
-const IconFigure = styled.figure`
-  width: 40px;
-`
-
-const StyledImage = styled(Image)`
-  width: 100%;
-  height: 100%;
-  object-fit: 'contain';
-  aspect-ratio: 16 / 9;
-`
-
-const TextWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  width: fit-content;
-  background-color: ${props => props.theme.colors.blue};
-  margin-top: 5px;
-  margin-left: auto;
-  margin-right: auto;
-  padding-top: 10px;
-  padding-bottom: 10px;
-  padding-left: 20px;
-  padding-right: 20px;
-  border-radius: 4px;
-  border: none;
-  transition: 200ms ease;
-
-  ${LinkWrapper}:hover & {
-    background-color: ${props => props.theme.colors.blueHover};
-  }
-`
-
-const Span = styled.span`
-  font-size: ${props => props.theme.fontSizes.small};
-  color: ${props => props.theme.colors.white};
-  font-weight: ${props => props.theme.fontWeights.bold};
-`
