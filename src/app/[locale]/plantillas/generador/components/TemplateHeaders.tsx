669407373d783f6cb3b606db30733880e38f1d14
desarrollocb <desarrollo01@cbinformatica.net>
2025-05-26 12:02:36 -0600

    Template generator tailwind

--- src/app/[locale]/plantillas/generador/components/TemplateHeaders.tsx
+++ src/app/[locale]/plantillas/generador/components/TemplateHeaders.tsx
@@ -1,38 +1,21 @@
-"use client"
-import styled from "styled-components"
 import { useTemplateContext } from "./TemplateContext"
 
 export default function TemplateHeaders() {
   const { titles } = useTemplateContext()
 
   return (
-    <Headers>
+    <div className="flex w-11/12 mx-auto">
       {
-        titles.map((title, index) => (
-          <Title key={index} $marginRight={title.marginRight}>{ title.name }</Title>
-        ))
+        titles.map((title, index) => {
+          const titleStyles = {
+            marginRight: `${title.marginRight}mm`,
+          }
+
+          return (
+            <h2 key={index} className="m-0 p-0" style={titleStyles}>{ title.name }</h2>
+          )
+        })
       }
-    </Headers>
+    </div>
   )
 }
-
-const Headers = styled.div`
-  display: flex;
-  width: 90%;
-  margin: 0 auto;
-`
-
-// const SubHeaders = styled.div`
-//   display: flex;
-//   flex-direction: row;
-//   gap: 30mm;
-//   margin-right: 10mm;
-//   margin-left: auto;
-// `
-
-const Title = styled.h2<{ $marginRight?: number}>`
-  margin: 0;
-  padding: 0;
-
-  margin-right: ${props => `${props.$marginRight}mm` || `${0}mm`};
-`
