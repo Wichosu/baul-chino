8cd059e6e7b467439547ebc51b5d658bbd5841a8
Wichosu <54341712+Wichosu@users.noreply.github.com>
2025-08-11 07:41:02 -0600

    New canonical /en

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -35,9 +35,8 @@ export async function generateMetadata({
     },
     metadataBase: new URL('https://www.baulchino.com/'),
     alternates: {
-      canonical: '/',
+      canonical: '/en',
       languages: {
-        en: '/en',
         es: '/es',
       },
     },
