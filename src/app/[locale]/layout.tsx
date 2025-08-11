631b32dcb8d1498cb8ebc75fc49b7397f6099a6b
Wichosu <54341712+Wichosu@users.noreply.github.com>
2025-08-10 20:56:07 -0600

    Change metadatabase url

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,46 +1,49 @@
-import type { Metadata } from "next";
-import { Montserrat } from "next/font/google";
-import { Analytics } from "@vercel/analytics/react"
+import type { Metadata } from 'next';
+import { Montserrat } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
 import '@/src/styles/globals.css';
-import { PostHogProvider } from "../providers";
-import { SpeedInsights } from "@vercel/speed-insights/next";
-import { NextIntlClientProvider, hasLocale } from "next-intl";
-import { getTranslations } from "next-intl/server";
-import { routing } from "@/src/i18n/routing";
-import { notFound } from "next/navigation";
-import { MobileNavbar, Navbar } from "@/src/app/components/Navbar";
-import Footer from "../components/Footer";
-import { Container } from "@/src/app/components/Container";
+import { PostHogProvider } from '../providers';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { NextIntlClientProvider, hasLocale } from 'next-intl';
+import { getTranslations } from 'next-intl/server';
+import { routing } from '@/src/i18n/routing';
+import { notFound } from 'next/navigation';
+import { MobileNavbar, Navbar } from '@/src/app/components/Navbar';
+import Footer from '../components/Footer';
+import { Container } from '@/src/app/components/Container';
 
 const montserrat = Montserrat({
-  subsets: ["latin"],
-})
+  subsets: ['latin'],
+});
 
-export async function generateMetadata({params}: {params: Promise<{locale: string}>}): Promise<Metadata> {
-  const { locale } = await params
-  const t = await getTranslations({locale, namespace: 'HomePage.Metadata'})
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}): Promise<Metadata> {
+  const { locale } = await params;
+  const t = await getTranslations({ locale, namespace: 'HomePage.Metadata' });
 
   return {
     title: {
       absolute: t('Title'),
-      template: "%s - Baúl Chino"
+      template: '%s - Baúl Chino',
     },
     description: t('Description'),
     twitter: {
-      card: "summary_large_image"
+      card: 'summary_large_image',
     },
-    metadataBase: new URL("https://www.baulchino.com/")
-  }
+    metadataBase: new URL('https://www.baulchino.com/en'),
+  };
 }
 
 export default async function RootLayout({
   children,
-  params
+  params,
 }: Readonly<{
   children: React.ReactNode;
-  params: Promise<{locale: string}>
+  params: Promise<{ locale: string }>;
 }>) {
-
   const { locale } = await params;
 
   if (!hasLocale(routing.locales, locale)) {
@@ -52,16 +55,14 @@ export default async function RootLayout({
       <html lang={locale}>
         <body className={montserrat.className}>
           <PostHogProvider>
-              <NextIntlClientProvider>
-                  <Container>
-                    <Navbar />
-                    <MobileNavbar />
-                    <main>
-                      {children}
-                    </main>
-                    <Footer />
-                  </Container>
-              </NextIntlClientProvider>
+            <NextIntlClientProvider>
+              <Container>
+                <Navbar />
+                <MobileNavbar />
+                <main>{children}</main>
+                <Footer />
+              </Container>
+            </NextIntlClientProvider>
           </PostHogProvider>
           <Analytics />
           <SpeedInsights />
