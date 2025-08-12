3469173530fe87edb1a0e8137d0e95875836c11f
desarrollocb <desarrollo01@cbinformatica.net>
2025-08-11 18:22:27 -0600

    Canonical and alternate ref for pages

--- src/app/[locale]/plantillas/generador/page.tsx
+++ src/app/[locale]/plantillas/generador/page.tsx
@@ -1,21 +1,34 @@
-import { Hero } from "@/src/app/components/Hero"
-import TemplateGenerator from "./components/TemplateGenerator"
-import { getTranslations } from "next-intl/server"
-import { Metadata } from "next"
-import { useTranslations } from "next-intl"
-import { languageList } from "@/src/app/utils/languages/languageList"
+import { Hero } from '@/src/app/components/Hero';
+import TemplateGenerator from './components/TemplateGenerator';
+import { getTranslations } from 'next-intl/server';
+import { Metadata } from 'next';
+import { useTranslations } from 'next-intl';
+import { languageList } from '@/src/app/utils/languages/languageList';
 
-export async function generateMetadata({params}: {params: Promise<{locale: string}>}): Promise<Metadata> {
-  const { locale } = await params
-  const t = await getTranslations({locale, namespace: 'TemplateGenerator.Metadata'})
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}): Promise<Metadata> {
+  const { locale } = await params;
+  const t = await getTranslations({
+    locale,
+    namespace: 'TemplateGenerator.Metadata',
+  });
 
   return {
     title: t('Title'),
     description: t('Description'),
     twitter: {
-      card: "summary_large_image"
+      card: 'summary_large_image',
     },
-  }
+    alternates: {
+      canonical: '/en',
+      languages: {
+        es: '/es',
+      },
+    },
+  };
 }
 
 export async function generateStaticParams() {
@@ -25,19 +38,15 @@ export async function generateStaticParams() {
 }
 
 export default function Page() {
-  const t = useTranslations('TemplateGenerator')
+  const t = useTranslations('TemplateGenerator');
 
   return (
     <>
       <Hero title={t('HeroTitle')}>
-        <span style={{ display: "block" }}>
-          {t('HeroMessage1')}
-        </span>
-        <span style={{ display: "block" }}>
-          {t('HeroMessage2')}
-        </span>
+        <span style={{ display: 'block' }}>{t('HeroMessage1')}</span>
+        <span style={{ display: 'block' }}>{t('HeroMessage2')}</span>
       </Hero>
       <TemplateGenerator />
     </>
-  )
+  );
 }
