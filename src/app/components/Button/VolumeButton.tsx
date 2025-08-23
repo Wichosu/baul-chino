c45d5fa98602952c1aa0c46202f8eee1f155a5f3
desarrollocb <desarrollo01@cbinformatica.net>
2025-08-22 18:18:33 -0600

    Audio component satisfied, need refactor

--- src/app/components/Button/VolumeButton.tsx
+++ src/app/components/Button/VolumeButton.tsx
@@ -3,9 +3,18 @@ import React from 'react';
 import { Button } from './Button';
 import { Volume, Volume1, Volume2, VolumeOff } from 'lucide-react';
 
-export function VolumeButton() {
+type Props = {
+  handleVolume: (volume: number) => void;
+};
+
+export function VolumeButton({ handleVolume }: Props) {
   const [volume, setVolume] = React.useState(100);
   const prevVolume = React.useRef(100);
+
+  React.useEffect(() => {
+    const percentageOfVolume = volume / 100;
+    handleVolume(percentageOfVolume);
+  }, [volume, handleVolume]);
 
   function handleInput(event: React.ChangeEvent<HTMLInputElement>) {
     const newVolume = parseInt(event.target.value);
@@ -24,7 +33,13 @@ export function VolumeButton() {
 
   return (
     <div className='group flex gap-2 rounded-md bg-yellow-200'>
-      <Button padding='1' margin='none' onClick={handleButton} className='peer'>
+      <Button
+        type='yellow'
+        padding='1'
+        margin='none'
+        onClick={handleButton}
+        className='peer'
+      >
         {volume >= 60 && <Volume2 />}
         {volume >= 20 && volume < 60 && <Volume1 />}
         {volume >= 1 && volume < 20 && <Volume />}
@@ -38,11 +53,11 @@ export function VolumeButton() {
         min='0'
         max='100'
         className='
-        w-0 -ml-4 transition-all delay-100 duration-500 ease-in-out opacity-0 cursor-pointer accent-yellow-800
-        group-hover:w-24 group-hover:ml-0 group-hover:opacity-100
-        group-focus:w-24 group-focus:ml-0 group-focus:opacity-100
-        peer-focus:w-24 peer-focus:ml-0 peer-focus:opacity-100
-        focus:w-24 focus:ml-0 focus:opacity-100
+          w-0 -ml-4 transition-all delay-100 duration-500 ease-in-out opacity-0 cursor-pointer accent-yellow-800
+          group-hover:w-24 group-hover:ml-0 group-hover:opacity-100
+          group-focus:w-24 group-focus:ml-0 group-focus:opacity-100
+          peer-focus:w-24 peer-focus:ml-0 peer-focus:opacity-100
+          focus:w-24 focus:ml-0 focus:opacity-100
         '
       />
       <div />
