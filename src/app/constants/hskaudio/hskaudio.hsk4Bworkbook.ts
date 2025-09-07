878ed09e0aa8c3bae7308dea5658cfb88b72a33b
Wichosu <luiscalderonmiranda@gmail.com>
2025-09-07 14:49:38 -0600

    hsk 4b added

---
+++ src/app/constants/hskaudio/hskaudio.hsk4Bworkbook.ts
@@ -0,0 +1,186 @@
+import {
+  Book,
+  createBook,
+  createLesson,
+  createAudioTrack,
+} from './hskaudio.types';
+
+export const hsk4Bworkbook: Book = createBook('HSK 4下 WORKBOOK', [
+  createLesson('HSK 4下 WORKBOOK - Lesson 11', [
+    createAudioTrack(
+      'Lesson 11 - 01',
+      'hsk4B-workbook-1101',
+      'pmZgVpOF4Srl9QFqlkvWrCbwga0Eon13fuDK6UpN5vBkXtSh'
+    ),
+    createAudioTrack(
+      'Lesson 11 - 02',
+      'hsk4B-workbook-1102',
+      'pmZgVpOF4SrlfaTjab6UedIATMvN4WYEa10X8Px6gmt35Gju'
+    ),
+    createAudioTrack(
+      'Lesson 11 - 03',
+      'hsk4B-workbook-1103',
+      'pmZgVpOF4SrlXdVtMO34Y3w0Qra2OzjPehCtZigGfvKBIADT'
+    ),
+  ]),
+  createLesson('HSK 4下 WORKBOOK - Lesson 12', [
+    createAudioTrack(
+      'Lesson 12 - 01',
+      'hsk4B-workbook-1201',
+      'pmZgVpOF4SrlFoHYe8JgUXwTkC6nPJSeaI1myoicBHDrlxZM'
+    ),
+    createAudioTrack(
+      'Lesson 12 - 02',
+      'hsk4B-workbook-1202',
+      'pmZgVpOF4Srl3vL0pzhJNBqAgPs8ewupM2VdG0FkatylLhD6'
+    ),
+    createAudioTrack(
+      'Lesson 12 - 03',
+      'hsk4B-workbook-1203',
+      'pmZgVpOF4SrlxQE4NPa7HiqY7Kjn5WpDFtb3IfAsLG8go4XB'
+    ),
+  ]),
+  createLesson('HSK 4下 WORKBOOK - Lesson 13', [
+    createAudioTrack(
+      'Lesson 13 - 01',
+      'hsk4B-workbook-1301',
+      'pmZgVpOF4SrlmMS8zCabLuNBQHvY6OiKgRnMkDq4X7A3jhl8'
+    ),
+    createAudioTrack(
+      'Lesson 13 - 02',
+      'hsk4B-workbook-1302',
+      'pmZgVpOF4Srl9KYBMQvWrCbwga0Eon13fuDK6UpN5vBkXtSh'
+    ),
+    createAudioTrack(
+      'Lesson 13 - 03',
+      'hsk4B-workbook-1303',
+      'pmZgVpOF4SrlaBeAmPG69n1wbW580GxDg23iyutEadrClqVF'
+    ),
+  ]),
+  createLesson('HSK 4下 WORKBOOK - Lesson 14', [
+    createAudioTrack(
+      'Lesson 14 - 01',
+      'hsk4B-workbook-1401',
+      'pmZgVpOF4Srlp4M8vGOF4Srl7icpkDXQGNIjeoxgmT6KCyfd'
+    ),
+    createAudioTrack(
+      'Lesson 14 - 02',
+      'hsk4B-workbook-1402',
+      'pmZgVpOF4SrlYoFGKW8BJ5gcweDIWO9GXKNz3iq7vyroRQa0'
+    ),
+    createAudioTrack(
+      'Lesson 14 - 03',
+      'hsk4B-workbook-1403',
+      'pmZgVpOF4SrlYL1jkR8BJ5gcweDIWO9GXKNz3iq7vyroRQa0'
+    ),
+  ]),
+  createLesson('HSK 4下 WORKBOOK - Lesson 15', [
+    createAudioTrack(
+      'Lesson 15 - 01',
+      'hsk4B-workbook-1501',
+      'pmZgVpOF4SrlOHcObQCRU4rdsfetVKvJP8C6NT1u39wcYbqz'
+    ),
+    createAudioTrack(
+      'Lesson 15 - 02',
+      'hsk4B-workbook-1502',
+      'pmZgVpOF4Srl1xYZPc0JUV4TINZhg0RL6BbPiWkFrGD5tj2n'
+    ),
+    createAudioTrack(
+      'Lesson 15 - 03',
+      'hsk4B-workbook-1503',
+      'pmZgVpOF4Srl24y74dDSyXihkmPQfr0vjdRonIwVbNGzusgB'
+    ),
+  ]),
+  createLesson('HSK 4下 WORKBOOK - Lesson 16', [
+    createAudioTrack(
+      'Lesson 16 - 01',
+      'hsk4B-workbook-1601',
+      'pmZgVpOF4SrleT5Q1qorcXnZJdiTEBxPp3ONbDy0CAFWm4oj'
+    ),
+    createAudioTrack(
+      'Lesson 16 - 02',
+      'hsk4B-workbook-1602',
+      'pmZgVpOF4SrlKVvU16ox4LMPVdRjxETlAXfuJi9eQFgap80y'
+    ),
+    createAudioTrack(
+      'Lesson 16 - 03',
+      'hsk4B-workbook-1603',
+      'pmZgVpOF4SrlgEW54NFkGsUQaCdXWO7ou3iFmYHBZMApLxq8'
+    ),
+  ]),
+  createLesson('HSK 4下 WORKBOOK - Lesson 17', [
+    createAudioTrack(
+      'Lesson 17 - 01',
+      'hsk4B-workbook-1701',
+      'pmZgVpOF4SrlzUk5auKkMPXs0DgnORyhbHNL8ufCZl2eoQGt'
+    ),
+    createAudioTrack(
+      'Lesson 17 - 02',
+      'hsk4B-workbook-1702',
+      'pmZgVpOF4SrlKHKMimx4LMPVdRjxETlAXfuJi9eQFgap80yo'
+    ),
+    createAudioTrack(
+      'Lesson 17 - 03',
+      'hsk4B-workbook-1703',
+      'pmZgVpOF4SrlnCDI9o8p2i9uJc4gOLBEVtzodXQemjTIxb6S'
+    ),
+  ]),
+  createLesson('HSK 4下 WORKBOOK - Lesson 18', [
+    createAudioTrack(
+      'Lesson 18 - 01',
+      'hsk4B-workbook-1801',
+      'pmZgVpOF4SrlGaLMvmddX4PrIjmMkTu8ilWFtfpJBg1SnwRL'
+    ),
+    createAudioTrack(
+      'Lesson 18 - 02',
+      'hsk4B-workbook-1802',
+      'pmZgVpOF4Srl6D8vr10cY8hIZSMvtcP10dWEGwjAbaUg25Hp'
+    ),
+    createAudioTrack(
+      'Lesson 18 - 03',
+      'hsk4B-workbook-1803',
+      'pmZgVpOF4SrlXoY2IY34Y3w0Qra2OzjPehCtZigGfvKBIADT'
+    ),
+  ]),
+  createLesson('HSK 4下 WORKBOOK - Lesson 19', [
+    createAudioTrack(
+      'Lesson 19 - 01',
+      'hsk4B-workbook-1901',
+      'pmZgVpOF4Srlz7lD0ZaKkMPXs0DgnORyhbHNL8ufCZl2eoQG'
+    ),
+    createAudioTrack(
+      'Lesson 19 - 02',
+      'hsk4B-workbook-1902',
+      'pmZgVpOF4SrlOFMfcKrCRU4rdsfetVKvJP8C6NT1u39wcYbq'
+    ),
+    createAudioTrack(
+      'Lesson 19 - 03',
+      'hsk4B-workbook-1903',
+      'pmZgVpOF4SrlKWcWU6x4LMPVdRjxETlAXfuJi9eQFgap80yo'
+    ),
+  ]),
+  createLesson('HSK 4下 WORKBOOK - Lesson 20', [
+    createAudioTrack(
+      'Lesson 20 - 01',
+      'hsk4B-workbook-2001',
+      'pmZgVpOF4Srlco9AFO2qjXFOgTv782txDP9ZG0lUkhdbKifL'
+    ),
+    createAudioTrack(
+      'Lesson 20 - 02',
+      'hsk4B-workbook-2002',
+      'pmZgVpOF4SrlCmdc3Gg4Nl8DfCykJj0hU1AWL2waumV7eSQs'
+    ),
+    createAudioTrack(
+      'Lesson 20 - 03',
+      'hsk4B-workbook-2003',
+      'pmZgVpOF4SrlnC2njWPp2i9uJc4gOLBEVtzodXQemjTIxb6S'
+    ),
+  ]),
+  createLesson('HSK 4下 WORKBOOK - Lesson 21', [
+    createAudioTrack(
+      'Lesson 21',
+      'hsk4B-workbook-21',
+      'pmZgVpOF4SrlkITq3nfmv6c38lADLaKh0iErjGJ7tsSz1VXZ'
+    ),
+  ]),
+]);
