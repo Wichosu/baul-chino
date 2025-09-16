f43064f58b73cddaf6e71788dc769d4050e04b53
Wichosu <luiscalderonmiranda@gmail.com>
2025-09-16 08:59:06 -0600

    fix anki decks issue, no anki deck being download, now fixed :D

--- src/app/[locale]/mazos-anki/components/ListItem.tsx
+++ src/app/[locale]/mazos-anki/components/ListItem.tsx
@@ -1,17 +1,18 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 interface Props {
-  linkRef: string,
-  linkName: string,
+  linkRef: string;
+  linkName: string;
 }
 
 export default function ListItem({ linkRef, linkName }: Props) {
   return (
-    <Link 
-      href={linkRef} 
-      className="text-xl text-blue-600 font-medium block my-5 w-fit no-underline hover:text-blue-700 hover:underline"
+    <Link
+      href={linkRef}
+      download
+      className='text-xl text-blue-600 font-medium block my-5 w-fit no-underline hover:text-blue-700 hover:underline'
     >
-      { linkName }
+      {linkName}
     </Link>
-  )
+  );
 }
