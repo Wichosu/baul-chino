febfbafeef6b203bce208d7d12293f5a1df9fbc7
Wichosu <luiscalderonmiranda@gmail.com>
2025-09-30 18:54:15 -0600

    Navbar center, key indexes with text, new navbar in layout

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,14 +1,12 @@
 import { useTranslations } from 'next-intl';
 import { Hero } from '../components/Hero';
 import CardsContainer from '../components/CardsContainer';
-import { Navbar } from '../components/Navbar/index';
 
 export default function Home() {
   const t = useTranslations('HomePage');
 
   return (
     <>
-      <Navbar />
       <Hero title={t('HeroTitle')}>{t('HeroMessage')}</Hero>
       <CardsContainer />
     </>
--- src/app/components/Navbar/NavbarViewport.tsx
+++ src/app/components/Navbar/NavbarViewport.tsx
@@ -3,7 +3,7 @@ import { NavigationMenu } from 'radix-ui';
 
 export function NavbarViewport() {
   return (
-    <div className='perspective-[2000px] absolute left-0 top-full flex w-full justify-start'>
+    <div className='perspective-[2000px] absolute left-0 top-full flex w-full justify-center'>
       <NavigationMenu.Viewport
         className='
           relative mt-2.5 h-[var(--radix-navigation-menu-viewport-height)] w-full 
