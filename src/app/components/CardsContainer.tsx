971d1f8bc6f568054092f18b9f4adf0bacf60752
Wichosu <luiscalderonmiranda@gmail.com>
2025-10-04 08:30:45 -0600

    Remove shadcn components

--- src/app/components/CardsContainer.tsx
+++ src/app/components/CardsContainer.tsx
@@ -1,6 +1,6 @@
 import { useTranslations } from 'next-intl';
 import Image from 'next/image';
-import Card from '@/src/app/components/NewCard';
+import Card from '@/src/app/components/Card';
 import { Button } from '@/src/app/components/Button';
 
 type Card = {
