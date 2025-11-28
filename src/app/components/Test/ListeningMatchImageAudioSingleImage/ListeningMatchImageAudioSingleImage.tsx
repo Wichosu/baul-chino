75189eefa4d3fc724a8622dca46df55e9e1190ec
Wichosu <luiscalderonmiranda@gmail.com>
2025-11-27 21:03:39 -0600

    Working on new section listening match image audio single image

---
+++ src/app/components/Test/ListeningMatchImageAudioSingleImage/ListeningMatchImageAudioSingleImage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function ListeningMatchImageAudioSingleImage() {
+    return (
+        <>
+            <h2>Listening Match Image Audio Single Image</h2>
+        </>
+    );
+}
