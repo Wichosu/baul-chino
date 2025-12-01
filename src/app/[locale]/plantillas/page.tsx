26a28d5c3e0f9369a4c15065584367b05aecbb17
Wichosu <luiscalderonmiranda@gmail.com>
2025-11-30 19:51:58 -0600

    forced SSG render in pages

--- src/app/[locale]/plantillas/page.tsx
+++ src/app/[locale]/plantillas/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = 'force-static';
 import { Metadata } from 'next';
 import { Hero } from '@/src/app/components/Hero';
 import Templates from './components/Templates/Templates';
