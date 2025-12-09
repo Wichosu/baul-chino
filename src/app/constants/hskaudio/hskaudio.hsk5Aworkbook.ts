c56aec4c971063b80476c98e1c2cd9eddc7092d3
Wichosu <luiscalderonmiranda@gmail.com>
2025-12-08 19:05:38 -0600

    update more audios to cloudflare r2

--- src/app/constants/hskaudio/hskaudio.hsk5Aworkbook.ts
+++ src/app/constants/hskaudio/hskaudio.hsk5Aworkbook.ts
@@ -10,216 +10,216 @@ export const hsk5Aworkbook: Book = createBook('HSK 5上 WORKBOOK', [
     createAudioTrack(
       'Lesson 01 - 01',
       'hsk5A-workbook-0101',
-      'pmZgVpOF4SrlQb4bNQW14SFyf85BlhTC1c6wtZJINpVuY3ED'
+      'https://cdn.baulchino.com/hsk-books-audios/hsk5a-workbook-audios/lesson-01/hsk5A-workbook-0101.mp3'
     ),
     createAudioTrack(
       'Lesson 01 - 02',
       'hsk5A-workbook-0102',
-      'pmZgVpOF4SrlLszWM9omW90sTr5AHGVMI6uSDowN2XnYiJ4g'
+      'https://cdn.baulchino.com/hsk-books-audios/hsk5a-workbook-audios/lesson-01/hsk5A-workbook-0102.mp3'
     ),
   ]),
   createLesson('HSK 5上 WORKBOOK - Lesson 02', [
     createAudioTrack(
       'Lesson 02 - 01',
       'hsk5A-workbook-0201',
-      'pmZgVpOF4SrlQMHwzP14SFyf85BlhTC1c6wtZJINpVuY3EDo'
+      'https://cdn.baulchino.com/hsk-books-audios/hsk5a-workbook-audios/lesson-02/hsk5A-workbook-0201.mp3'
     ),
     createAudioTrack(
       'Lesson 02 - 02',
       'hsk5A-workbook-0202',
-      'pmZgVpOF4SrlfRXpjQ6UedIATMvN4WYEa10X8Px6gmt35Gju'
+      'https://cdn.baulchino.com/hsk-books-audios/hsk5a-workbook-audios/lesson-02/hsk5A-workbook-0202.mp3'
     ),
   ]),
   createLesson('HSK 5上 WORKBOOK - Lesson 03', [
     createAudioTrack(
       'Lesson 03 - 01',
       'hsk5A-workbook-0301',
-      'pmZgVpOF4SrlCC1sNNg4Nl8DfCykJj0hU1AWL2waumV7eSQs'
+      'https://cdn.baulchino.com/hsk-books-audios/hsk5a-workbook-audios/lesson-03/hsk5A-workbook-0301.mp3'
     ),
     createAudioTrack(
       'Lesson 03 - 02',
       'hsk5A-workbook-0302',
-      'pmZgVpOF4Srlpe5fooOF4Srl7icpkDXQGNIjeoxgmT6KCyfd'
+      'https://cdn.baulchino.com/hsk-books-audios/hsk5a-workbook-audios/lesson-03/hsk5A-workbook-0302.mp3'
     ),
   ]),
   createLesson('HSK 5上 WORKBOOK - Lesson 04', [
     createAudioTrack(
       'Lesson 04 - 01',
       'hsk5A-workbook-0401',
-      'pmZgVpOF4SrlFucmwAzJgUXwTkC6nPJSeaI1myoicBHDrlxZ'
+      'https://cdn.baulchino.com/hsk-books-audios/hsk5a-workbook-audios/lesson-04/hsk5A-workbook-0401.mp3'
     ),
     createAudioTrack(
       'Lesson 04 - 02',
       'hsk5A-workbook-0402',
-      'pmZgVpOF4Srly6DdFVTkB7tDUVmTzWa5fin28J43MPxR91kZ'
+      'https://cdn.baulchino.com/hsk-books-audios/hsk5a-workbook-audios/lesson-04/hsk5A-workbook-0402.mp3'
     ),
   ]),
   createLesson('HSK 5上 WORKBOOK - Lesson 05', [
     createAudioTrack(
       'Lesson 05 - 01',
       'hsk5A-workbook-0501',
-      'pmZgVpOF4Srl2k5fcbDSyXihkmPQfr0vjdRonIwVbNGzusgB'
+      'https://cdn.baulchino.com/hsk-books-audios/hsk5a-workbook-audios/lesson-05/hsk5A-workbook-0501.mp3'
     ),
     createAudioTrack(
       'Lesson 05 - 02',
       'hsk5A-workbook-0502',
-      'pmZgVpOF4SrlGXcrXDddX4PrIjmMkTu8ilWFtfpJBg1SnwRL'
+      'https://cdn.baulchino.com/hsk-books-audios/hsk5a-workbook-audios/lesson-05/hsk5A-workbook-0502.mp3'
     ),
   ]),
   createLesson('HSK 5上 WORKBOOK - Lesson 06', [
     createAudioTrack(
       'Lesson 06 - 01',
       'hsk5A-workbook-0601',
-      'pmZgVpOF4SrldAzvWNqtCZiJeKXbk462AVEYNGQHlsyMcTzu'
+      'https://cdn.baulchino.com/hsk-books-audios/hsk5a-workbook-audios/lesson-06/hsk5A-workbook-0601.mp3'
     ),
     createAudioTrack(
       'Lesson 06 - 02',
       'hsk5A-workbook-0602',
-      'pmZgVpOF4SrlKV0zOPLx4LMPVdRjxETlAXfuJi9eQFgap80y'
+      'https://cdn.baulchino.com/hsk-books-audios/hsk5a-workbook-audios/lesson-06/hsk5A-workbook-0602.mp3'
     ),
   ]),
   createLesson('HSK 5上 WORKBOOK - Lesson 07', [
     createAudioTrack(
       'Lesson 07 - 01',
       'hsk5A-workbook-0701',
-      'pmZgVpOF4SrlJfI9yws6AN4vDiWjFztqaxymSZkrU2ehHpGo'
+      'https://cdn.baulchino.com/hsk-books-audios/hsk5a-workbook-audios/lesson-07/hsk5A-workbook-0701.mp3'
     ),
     createAudioTrack(
       'Lesson 07 - 02',
       'hsk5A-workbook-0702',
-      'pmZgVpOF4SrlFotxxjJgUXwTkC6nPJSeaI1myoicBHDrlxZM'
+      'https://cdn.baulchino.com/hsk-books-audios/hsk5a-workbook-audios/lesson-07/hsk5A-workbook-0702.mp3'
     ),
   ]),
   createLesson('HSK 5上 WORKBOOK - Lesson 08', [
     createAudioTrack(
       'Lesson 08 - 01',
       'hsk5A-workbook-0801',
-      'pmZgVpOF4Srlmd0zuSQabLuNBQHvY6OiKgRnMkDq4X7A3jhl'
+      'https://cdn.baulchino.com/hsk-books-audios/hsk5a-workbook-audios/lesson-08/hsk5A-workbook-0801.mp3'
     ),
     createAudioTrack(
       'Lesson 08 - 02',
       'hsk5A-workbook-0802',
-      'pmZgVpOF4Srl6DaDipJcY8hIZSMvtcP10dWEGwjAbaUg25Hp'
+      'https://cdn.baulchino.com/hsk-books-audios/hsk5a-workbook-audios/lesson-08/hsk5A-workbook-0802.mp3'
     ),
   ]),
   createLesson('HSK 5上 WORKBOOK - Lesson 09', [
     createAudioTrack(
       'Lesson 09 - 01',
       'hsk5A-workbook-0901',
-      'pmZgVpOF4SrlffZIWZ6UedIATMvN4WYEa10X8Px6gmt35Gju'
+      'https://cdn.baulchino.com/hsk-books-audios/hsk5a-workbook-audios/lesson-09/hsk5A-workbook-0901.mp3'
     ),
     createAudioTrack(
       'Lesson 09 - 02',
       'hsk5A-workbook-0902',
-      'pmZgVpOF4SrljnAzs7Pno5a6lc8qVOdFuyJZg4ibh2CkDtI1'
+      'https://cdn.baulchino.com/hsk-books-audios/hsk5a-workbook-audios/lesson-09/hsk5A-workbook-0902.mp3'
     ),
   ]),
   createLesson('HSK 5上 WORKBOOK - Lesson 10', [
     createAudioTrack(
       'Lesson 10 - 01',
       'hsk5A-workbook-1001',
-      'pmZgVpOF4Srlkc2sUCMfmv6c38lADLaKh0iErjGJ7tsSz1VX'
+      'https://cdn.baulchino.com/hsk-books-audios/hsk5a-workbook-audios/lesson-10/hsk5A-workbook-1001.mp3'
     ),
     createAudioTrack(
       'Lesson 10 - 02',
       'hsk5A-workbook-1002',
-      'pmZgVpOF4Srl2SGHyyDSyXihkmPQfr0vjdRonIwVbNGzusgB'
+      'https://cdn.baulchino.com/hsk-books-audios/hsk5a-workbook-audios/lesson-10/hsk5A-workbook-1002.mp3'
     ),
   ]),
   createLesson('HSK 5上 WORKBOOK - Lesson 11', [
     createAudioTrack(
       'Lesson 11 - 01',
       'hsk5A-workbook-1101',
-      'pmZgVpOF4Srl4wl2W59xbZ0AwPmY1VJH2ofWCidn8aDyNg3G'
+      'https://cdn.baulchino.com/hsk-books-audios/hsk5a-workbook-audios/lesson-11/hsk5A-workbook-1101.mp3'
     ),
     createAudioTrack(
       'Lesson 11 - 02',
       'hsk5A-workbook-1102',
-      'pmZgVpOF4Srl0nfHNCbVcr26vebq9QF3UCj4XkgPNTEYtO7S'
+      'https://cdn.baulchino.com/hsk-books-audios/hsk5a-workbook-audios/lesson-11/hsk5A-workbook-1102.mp3'
     ),
   ]),
   createLesson('HSK 5上 WORKBOOK - Lesson 12', [
     createAudioTrack(
       'Lesson 12 - 01',
       'hsk5A-workbook-1201',
-      'pmZgVpOF4Srlpi9uW8OF4Srl7icpkDXQGNIjeoxgmT6KCyfd'
+      'https://cdn.baulchino.com/hsk-books-audios/hsk5a-workbook-audios/lesson-12/hsk5A-workbook-1201.mp3'
     ),
     createAudioTrack(
       'Lesson 12 - 02',
       'hsk5A-workbook-1202',
-      'pmZgVpOF4SrlUe39tOApa8b0cLnkoHqhDBOsAFuK67fEtdJe'
+      'https://cdn.baulchino.com/hsk-books-audios/hsk5a-workbook-audios/lesson-12/hsk5A-workbook-1202.mp3'
     ),
   ]),
   createLesson('HSK 5上 WORKBOOK - Lesson 13', [
     createAudioTrack(
       'Lesson 13 - 01',
       'hsk5A-workbook-1301',
-      'pmZgVpOF4SrlMCPVGrq6bQoU5uI0tWpyNrfd74AGKhDznm8k'
+      'https://cdn.baulchino.com/hsk-books-audios/hsk5a-workbook-audios/lesson-13/hsk5A-workbook-1301.mp3'
     ),
     createAudioTrack(
       'Lesson 13 - 02',
       'hsk5A-workbook-1302',
-      'pmZgVpOF4Srlf8spcD6UedIATMvN4WYEa10X8Px6gmt35Gju'
+      'https://cdn.baulchino.com/hsk-books-audios/hsk5a-workbook-audios/lesson-13/hsk5A-workbook-1302.mp3'
     ),
   ]),
   createLesson('HSK 5上 WORKBOOK - Lesson 14', [
     createAudioTrack(
       'Lesson 14 - 01',
       'hsk5A-workbook-1401',
-      'pmZgVpOF4SrlKvDXyvx4LMPVdRjxETlAXfuJi9eQFgap80yo'
+      'https://cdn.baulchino.com/hsk-books-audios/hsk5a-workbook-audios/lesson-14/hsk5A-workbook-1401.mp3'
     ),
     createAudioTrack(
       'Lesson 14 - 02',
       'hsk5A-workbook-1402',
-      'pmZgVpOF4SrlocT3E7mqr0wIBi26ZcySMaFCEkQoWgsbt4mh'
+      'https://cdn.baulchino.com/hsk-books-audios/hsk5a-workbook-audios/lesson-14/hsk5A-workbook-1402.mp3'
     ),
   ]),
   createLesson('HSK 5上 WORKBOOK - Lesson 15', [
     createAudioTrack(
       'Lesson 15 - 01',
       'hsk5A-workbook-1501',
-      'pmZgVpOF4SrlYX6Mlz8BJ5gcweDIWO9GXKNz3iq7vyroRQa0'
+      'https://cdn.baulchino.com/hsk-books-audios/hsk5a-workbook-audios/lesson-15/hsk5A-workbook-1501.mp3'
     ),
     createAudioTrack(
       'Lesson 15 - 02',
       'hsk5A-workbook-1502',
-      'pmZgVpOF4SrloTirYemqr0wIBi26ZcySMaFCEkQoWgsbt4mh'
+      'https://cdn.baulchino.com/hsk-books-audios/hsk5a-workbook-audios/lesson-15/hsk5A-workbook-1502.mp3'
     ),
   ]),
   createLesson('HSK 5上 WORKBOOK - Lesson 16', [
     createAudioTrack(
       'Lesson 16 - 01',
       'hsk5A-workbook-1601',
-      'pmZgVpOF4SrlHtmlFxMOQ91zmte5E32XyjIgJUFGf0VqS7nr'
+      'https://cdn.baulchino.com/hsk-books-audios/hsk5a-workbook-audios/lesson-16/hsk5A-workbook-1601.mp3'
     ),
     createAudioTrack(
       'Lesson 16 - 02',
       'hsk5A-workbook-1602',
-      'pmZgVpOF4SrltntEbJUTzkMXia8ubjcUR46DSAEGJntfQsVl'
+      'https://cdn.baulchino.com/hsk-books-audios/hsk5a-workbook-audios/lesson-16/hsk5A-workbook-1602.mp3'
     ),
   ]),
   createLesson('HSK 5上 WORKBOOK - Lesson 17', [
     createAudioTrack(
       'Lesson 17 - 01',
       'hsk5A-workbook-1701',
-      'pmZgVpOF4SrlnCyj35yp2i9uJc4gOLBEVtzodXQemjTIxb6S'
+      'https://cdn.baulchino.com/hsk-books-audios/hsk5a-workbook-audios/lesson-17/hsk5A-workbook-1701.mp3'
     ),
     createAudioTrack(
       'Lesson 17 - 02',
       'hsk5A-workbook-1702',
-      'pmZgVpOF4SrldHMDQPtCZiJeKXbk462AVEYNGQHlsyMcTzu8'
+      'https://cdn.baulchino.com/hsk-books-audios/hsk5a-workbook-audios/lesson-17/hsk5A-workbook-1702.mp3'
     ),
   ]),
   createLesson('HSK 5上 WORKBOOK - Lesson 18', [
     createAudioTrack(
       'Lesson 18 - 01',
       'hsk5A-workbook-1801',
-      'pmZgVpOF4SrllPnGWseHM50UaZTLD93wuroWtXgfIQcshY86'
+      'https://cdn.baulchino.com/hsk-books-audios/hsk5a-workbook-audios/lesson-18/hsk5A-workbook-1801.mp3'
     ),
     createAudioTrack(
       'Lesson 18 - 02',
       'hsk5A-workbook-1802',
-      'pmZgVpOF4SrlG7UsO7ddX4PrIjmMkTu8ilWFtfpJBg1SnwRL'
+      'https://cdn.baulchino.com/hsk-books-audios/hsk5a-workbook-audios/lesson-18/hsk5A-workbook-1802.mp3'
     ),
   ]),
 ]);
