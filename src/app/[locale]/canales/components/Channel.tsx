6e003f8a11577a7e4575f16df9c92b7aa06cd053
Wichosu <luiscalderonmiranda@gmail.com>
2026-01-29 15:24:50 -0600

    format Channel.tsx, styles cannot be apply since it's url that breaks the responsiveness

--- src/app/[locale]/canales/components/Channel.tsx
+++ src/app/[locale]/canales/components/Channel.tsx
@@ -1,32 +1,38 @@
-import Link from "next/link"
-import { IChannel } from "../interfaces/IChannel"
-import React from "react"
+import Link from 'next/link';
+import { IChannel } from '../interfaces/IChannel';
+import React from 'react';
 
 interface Props {
-  channel: IChannel
+  channel: IChannel;
 }
 
-export default function Channel ({ channel }: Props) {
+export default function Channel({ channel }: Props) {
   return (
-    <article className="my-10">
+    <article className='my-10'>
       <Link
         href={channel.url}
-        target="_blank" 
-        className="text-2xl text-blue-600 font-medium no-underline hover:underline hover:text-blue-700"
+        target='_blank'
+        className='text-2xl text-blue-600 font-medium no-underline hover:underline hover:text-blue-700'
       >
-        { channel.name }
+        {channel.name}
       </Link>
-      <p className="text-xl text-black font-normal my-2.5">{ channel.description }</p>
-      <Categories>{ channel.channel_category.map(({ category }) => `${category.name} | `)}</Categories>
-      <Categories>{ channel.channel_language.map(({ language }) => `${language.name} | `)}</Categories>
+      <p className='text-xl text-black font-normal my-2.5'>
+        {channel.description}
+      </p>
+      <Categories>
+        {channel.channel_category.map(({ category }) => `${category.name} | `)}
+      </Categories>
+      <Categories>
+        {channel.channel_language.map(({ language }) => `${language.name} | `)}
+      </Categories>
     </article>
-  )
+  );
 }
 
-function Categories ({ children }: { children: React.ReactNode }) {
+function Categories({ children }: { children: React.ReactNode }) {
   return (
-    <small className="text-lg text-black font-light block mb-1">
-      { children }
+    <small className='text-lg text-black font-light block mb-1'>
+      {children}
     </small>
-  )
+  );
 }
