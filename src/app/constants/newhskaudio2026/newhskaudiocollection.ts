3903b4edf79b0b3cd7ba5fa6a060e9561f6bad91
Wichosu <luiscalderonmiranda@gmail.com>
2026-04-03 20:51:46 -0600

    added audio files objects

--- src/app/constants/newhskaudio2026/newhskaudiocollection.ts
+++ src/app/constants/newhskaudio2026/newhskaudiocollection.ts
@@ -1,4 +1,10 @@
 import { Book } from './newhskaudio.types';
 import { newhsk1textbook } from './newhskaudio.hsk1textbook';
+import { newhsk2textbook } from './newhskaudio.hsk2textbook';
+import { newhsk3textbook } from './newhskaudio.hsk3textbook';
 
-export const Books: Book[] = [newhsk1textbook];
+export const Books: Book[] = [
+  newhsk1textbook,
+  newhsk2textbook,
+  newhsk3textbook,
+];
