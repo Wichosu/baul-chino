caf0d6b31efc76d6094175d5efd9cc7025498a4a
Wichosu <luiscalderonmiranda@gmail.com>
2026-04-12 20:30:25 -0600

    working on new-hsk-mock-test-2026 page

--- src/app/[locale]/hsk-mock-test/page.tsx
+++ src/app/[locale]/hsk-mock-test/page.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { Metadata } from 'next';
-import { getTranslations } from 'next-intl/server';
-import { useTranslations } from 'next-intl';
-import { Hero } from '@/src/app/components/Hero';
-import { Card } from '@/src/app/components/Card';
-import { Button } from '@/src/app/components/Button';
-import { MockTestBundles } from '@/src/app/constants/hskmocktest/hskmocktestbundles';
-import { MockTestCardFetch } from '@/src/app/components/MockTestCardFetch';
-import { CardSkeleton } from '@/src/app/components/Card';
+import React from "react";
+import { Metadata } from "next";
+import { getTranslations } from "next-intl/server";
+import { useTranslations } from "next-intl";
+import { Hero } from "@/src/app/components/Hero";
+import { Card } from "@/src/app/components/Card";
+import { Button } from "@/src/app/components/Button";
+import { MockTestBundles } from "@/src/app/constants/hskmocktest/hskmocktestbundles";
+import { MockTestCardFetch } from "@/src/app/components/MockTestCardFetch";
+import { CardSkeleton } from "@/src/app/components/Card";
 
 export async function generateMetadata({
   params,
@@ -17,55 +17,55 @@ export async function generateMetadata({
   const { locale } = await params;
   const t = await getTranslations({
     locale,
-    namespace: 'HskMockTest.Metadata',
+    namespace: "HskMockTest.Metadata",
   });
 
   return {
-    title: t('Title'),
-    description: t('Description'),
+    title: t("Title"),
+    description: t("Description"),
     twitter: {
-      card: 'summary_large_image',
+      card: "summary_large_image",
     },
     alternates: {
-      canonical: '/hsk-mock-test',
+      canonical: "/hsk-mock-test",
       languages: {
-        es: '/es/hsk-mock-test',
+        es: "/es/hsk-mock-test",
       },
     },
   };
 }
 
 export default function Page() {
-  const t = useTranslations('HskMockTest');
+  const t = useTranslations("HskMockTest");
 
   return (
     <>
-      <Hero title={t('HeroTitle')}>
-        <span className='block'>{t('HeroMessage1')}</span>
-        <span className='block mt-4'>{t('HeroMessage2')}</span>
+      <Hero title={t("HeroTitle")}>
+        <span className="block">{t("HeroMessage1")}</span>
+        <span className="block mt-4">{t("HeroMessage2")}</span>
       </Hero>
       <section>
-        <h2 className='text-3xl font-medium'>{t('OnlineSimulator')}</h2>
-        <div className='flex flex-wrap justify-center'>
-          <React.Suspense fallback={<CardSkeleton height='xs' />}>
+        <h2 className="text-3xl font-medium">{t("OnlineSimulator")}</h2>
+        <div className="flex flex-wrap justify-center">
+          <React.Suspense fallback={<CardSkeleton height="xs" />}>
             <MockTestCardFetch />
           </React.Suspense>
         </div>
-        <h2 className='text-3xl font-medium'>{t('DownloadBundles')}</h2>
-        <div className='flex flex-wrap justify-center'>
+        <h2 className="text-3xl font-medium">{t("DownloadBundles")}</h2>
+        <div className="flex flex-wrap justify-center">
           {MockTestBundles.map((bundle, index) => (
-            <Card key={`bundle-${index}`} as='article' padding='3'>
-              <h3 className='text-2xl font-medium text-center'>
+            <Card key={`bundle-${index}`} as="article" padding="3">
+              <h3 className="text-2xl font-medium text-center">
                 HSK {index + 1}
               </h3>
-              <div className='flex flex-col'>
+              <div className="flex flex-col">
                 {bundle.map((item) => (
                   <Button
                     key={item.filename}
-                    type='yellow'
-                    as='link'
+                    type="yellow"
+                    as="link"
                     href={item.url}
-                    target='_self'
+                    target="_self"
                   >
                     {item.title}
                   </Button>
